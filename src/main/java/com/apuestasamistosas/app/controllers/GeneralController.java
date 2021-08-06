2c176d1fde063501059e5546f0ed49dd5eefa0c1
locknet11 <locknet41@protonmail.com>
2021-08-05 21:20:28 -0300

    Update GeneralController.java

--- src/main/java/com/apuestasamistosas/app/controllers/GeneralController.java
+++ src/main/java/com/apuestasamistosas/app/controllers/GeneralController.java
@@ -1,7 +1,17 @@
 
 package com.apuestasamistosas.app.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping
 public class GeneralController {
+    
+    @GetMapping()
+    public String index(){
+        return "rama santiago";
+    }
 
 }
