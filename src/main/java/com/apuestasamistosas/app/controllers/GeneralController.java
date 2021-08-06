f6643199cd32f12677d9414d7fbb1532dc210385
locknet11 <locknet41@protonmail.com>
2021-08-05 21:26:57 -0300

    corregi error

--- src/main/java/com/apuestasamistosas/app/controllers/GeneralController.java
+++ src/main/java/com/apuestasamistosas/app/controllers/GeneralController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class GeneralController {
-    
+
     @GetMapping()
     public String index(){
         return "rama santiago";
