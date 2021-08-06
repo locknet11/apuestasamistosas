4cdbebb9eb8a1d169917251954746348c101e77b
locknet11 <locknet41@protonmail.com>
2021-08-05 22:42:09 -0300

    Update GeneralController.java

--- src/main/java/com/apuestasamistosas/app/controllers/GeneralController.java
+++ src/main/java/com/apuestasamistosas/app/controllers/GeneralController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class GeneralController {
 
-    @GetMapping()
+    @GetMapping("/")
     public String index(){
         return "index";
     }
