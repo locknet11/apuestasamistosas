4354d0384d41919a7bbc6355f838884d65682bd7
locknet11 <locknet41@protonmail.com>
2021-08-09 21:14:43 -0300

    Update SecurityConfig.java and application.properties

--- src/main/java/com/apuestasamistosas/app/security/SecurityConfig.java
+++ src/main/java/com/apuestasamistosas/app/security/SecurityConfig.java
@@ -10,8 +10,4 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth){
-        
-    }
 }
