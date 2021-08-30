b3950cf73673d717444714fc6c7d76d8ed954efc
MartinMalnatti <malnattimartin@gmail.com>
2021-08-30 20:24:44 -0300

    APUESTA SERVICIO

--- src/main/java/com/apuestasamistosas/app/services/ApuestaServicio.java
+++ src/main/java/com/apuestasamistosas/app/services/ApuestaServicio.java
@@ -1,9 +1,17 @@
 
 package com.apuestasamistosas.app.services;
 
+import com.apuestasamistosas.app.repositories.ApuestaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApuestaServicio {
+    @Autowired
+    private ApuestaRepositorio apuestaRepositorio;
+    
+    public void  crearApuesta (){
+        
+    }
 
 }
